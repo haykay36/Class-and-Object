014b42871de8cd2f352e0e39a337c16adacd303d
akeem <akeemayinla36@gmail.com>
2024-01-28 08:17:28 -0500

    first commit message

---
+++ index.js
@@ -0,0 +1,18 @@
+class person{
+    Doctors = "A person who is qualified to treat ill people"
+    Engineers = "A person who build, design or maintain engine or structure"
+
+    //person is a CLASS. person1 and person2 are examples of class
+    static person1 = "A man that has two eyes"
+    static person2 = "A woman that has two legs"
+}
+// in initiation of object // person3 is an object
+//Doctors and Engineer are an example of an object
+const person3 = new person()
+console.log(person)
+console.log(person3)
+// Therefore a class is the blueprint or structural plan of an object
+//while an object is the instance of a class 
+//person: person1 and person2 are examples of class
+//doctors and engineers are object under the class person
+//every doctors and engineer can either be a man or a woman with two eyes and two legs.
